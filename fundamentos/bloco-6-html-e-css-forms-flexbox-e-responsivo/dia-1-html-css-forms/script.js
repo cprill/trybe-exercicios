b52bcfa49361c2444ac8c3e9ff4dbbc898117af8
Carlos Pabst Prillwitz <cprill@gmail.com>
2022-01-06 13:40:59 -0300

    Realizados

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -13,8 +13,16 @@ createStateOptions();
 
 
 function verificaDatas() {
-  if (datas.value[0] + datas.value[1] > 10) {
-    alert("Eu sou um alert!");
+  if (  datas.value[0] + datas.value[1] > 32) {
+    alert("Data incorreta!");
+  };
+
+  if ( datas.value[3] + datas.value[4] > 13) {
+    alert("Data incorreta!");
+  }
+
+  if ( datas.value[6] + datas.value[7] + datas.value[8] + datas.value[9] + datas.value[10] < 0) {
+    alert("Data incorreta!");
   } 
 
 }
