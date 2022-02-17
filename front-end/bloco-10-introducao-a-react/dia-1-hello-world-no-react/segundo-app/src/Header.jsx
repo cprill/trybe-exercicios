ee0ba11ae047b7e10e99cb702653472391814619
Carlos Pabst Prillwitz <cprill@gmail.com>
2022-02-17 15:03:39 -0300

    exercicio 2

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/segundo-app/src/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <h1>Conteudos</h1>
+        )
+    }
+}
+
+export default Header;
